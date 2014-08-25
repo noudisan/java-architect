3c9f3eb3bfa1acdbdc55cd2caa6a4ff55ffb4085
zhoutaotao <zhoutaotao@taotao>
2014-08-25 09:27:53 +0800

    redis 代码重构

--- src/main/java/com/ztt/redis/dao/impl/UserDaoImpl.java
+++ src/main/java/com/ztt/redis/dao/impl/UserDaoImpl.java
@@ -17,15 +17,17 @@ public class UserDaoImpl implements UserDao {
 	@Autowired
 	private RedisTemplate<Serializable, Serializable> redisTemplate;
 
+	private static final String PREFIX_UID ="user.uid.";
 	@Override
 	public void save(final User user) {
 		redisTemplate.execute(new RedisCallback<Object>() {
 			@Override
 			public Object doInRedis(RedisConnection connection)
 					throws DataAccessException {
-				connection.set(
-						redisTemplate.getStringSerializer().serialize("user.uid." + user.getUid()),
-						redisTemplate.getStringSerializer().serialize(user.getAddress()));
+				byte[] serializeUid = redisTemplate.getStringSerializer().serialize(PREFIX_UID+ user.getUid());
+				byte[] serializeAddress = redisTemplate.getStringSerializer().serialize(user.getAddress());
+				
+				connection.set(serializeUid,serializeAddress);
 				return null;
 			}
 		});
@@ -37,7 +39,7 @@ public class UserDaoImpl implements UserDao {
 			@Override
 			public User doInRedis(RedisConnection connection)
 					throws DataAccessException {
-				byte[] key = redisTemplate.getStringSerializer().serialize("user.uid." + uid);
+				byte[] key = redisTemplate.getStringSerializer().serialize(PREFIX_UID + uid);
 				if (connection.exists(key)) {
 					byte[] value = connection.get(key);
 					String address = redisTemplate.getStringSerializer().deserialize(value);
@@ -55,7 +57,7 @@ public class UserDaoImpl implements UserDao {
 	public void delete(final String uid) {
 		redisTemplate.execute(new RedisCallback<Object>() {
 			public Object doInRedis(RedisConnection connection) {
-				connection.del(redisTemplate.getStringSerializer().serialize("user.uid." + uid));
+				connection.del(redisTemplate.getStringSerializer().serialize(PREFIX_UID + uid));
 				return null;
 			}
 		});
