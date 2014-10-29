1202cf389e6c81915f6519d10289ceb062bd14ca
noudisan <zhou_ttaaoo@126.com>
2014-10-29 16:22:49 +0800

    mybatis add

--- src/test/java/com/ztt/test/ArchitectServiceTest.java
+++ src/test/java/com/ztt/test/ArchitectServiceTest.java
@@ -1,5 +1,24 @@
 package com.ztt.test;
 
 
-public class ArchitectServiceTest {
+import com.ztt.dao.ArchitectMapper;
+import com.ztt.dto.ArchitectSearchDto;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ArchitectServiceTest  extends BaseTest{
+
+    @Autowired
+    private ArchitectMapper architectMapper;
+
+    @Test
+    public void test_get(){
+        ArchitectSearchDto searchDto=new ArchitectSearchDto();
+
+        Integer count = architectMapper.count(searchDto);
+
+        Assert.assertNotNull(count);
+    }
+
 }
